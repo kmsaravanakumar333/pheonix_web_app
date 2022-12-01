50abd62c28b7ef9d58138b1e99b3cf0e3cbdc054
Parthiban Selvaraj <parthibanselvaraj4264@gmail.com>
2022-12-01 13:02:02 +0530

    updated all pages

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,7 +1,380 @@
-import React from 'react'
+import React from "react";
+import "./Services.css";
+
+import Uxui from "../../Assets/Ui ux development.jpg";
+import Webpage from "../../Assets/Webpage creation.jpg";
+import admin from "../../Assets/Admin panel management.jpg";
+import website from "../../Assets/All kind of websites.jpg";
+import wooCommerce from "../../Assets/Woocommerce.jpg";
+import mobileApp from "../../Assets/mobile app developement.jpg";
 
 export default function Services() {
   return (
-    <div>Services</div>
-  )
+    <section>
+      <div className="service-page">
+        <header>
+          <div className="service-page-main-1">
+            <div className="service-header">
+              <div className="service-text-box">
+                <h1 className="service-main-heading">
+                  our passion is your success
+                </h1>
+                <h6 className="service-sub-heading">
+                  We know that one size does not fit all. Our customer's success
+                  stems from making each employee recognition program match each
+                  organization, down to the details. We work to understand the
+                  uniqueness of your organization and vision so we can set up
+                  and support your success, everyday.
+                </h6>
+              </div>
+            </div>
+          </div>
+        </header>
+        <article>
+          <div className="service-page-main-2">
+            <div className="sub-service-page">
+              <div className="service-page-grid">
+                <div className="service-page-grid-items">
+                  <div className="service-page-grid-icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      className="icon-service-1"
+                      fill="#000000"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <line
+                        x1="104"
+                        y1="104"
+                        x2="104"
+                        y2="208"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                      <line
+                        x1="32"
+                        y1="104"
+                        x2="224"
+                        y2="104"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                      <rect
+                        x="32"
+                        y="48"
+                        width="192"
+                        height="160"
+                        rx="8"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></rect>
+                    </svg>
+                  </div>
+                  <div className="service-grid-icon-name">
+                    UI/UX Development
+                  </div>
+                </div>
+                <div className="service-page-grid-items">
+                  <div className="service-page-grid-icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      className="icon-service-1"
+                      fill="#000000"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <rect
+                        x="32"
+                        y="80"
+                        width="160"
+                        height="128"
+                        rx="8"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></rect>
+                      <path
+                        d="M64,80V56a8,8,0,0,1,8-8H216a8,8,0,0,1,8,8V168a8,8,0,0,1-8,8H192"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                      <line
+                        x1="32"
+                        y1="112"
+                        x2="192"
+                        y2="112"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                    </svg>
+                  </div>
+                  <div className="service-grid-icon-name">Webpage Creation</div>
+                </div>
+                <div className="service-page-grid-items">
+                  <div className="service-page-grid-icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      className="icon-service-1"
+                      fill="#000000"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <polyline
+                        points="160 80 192 80 192 112"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></polyline>
+                      <polyline
+                        points="96 176 64 176 64 144"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></polyline>
+                      <rect
+                        x="32"
+                        y="48"
+                        width="192"
+                        height="160"
+                        rx="8"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></rect>
+                    </svg>
+                  </div>
+                  <div className="service-grid-icon-name">
+                    Admin Panel Management
+                  </div>
+                </div>
+                <div className="service-page-grid-items">
+                  <div className="service-page-grid-icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      className="icon-service-1"
+                      fill="#000000"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <rect
+                        x="32"
+                        y="48"
+                        width="192"
+                        height="160"
+                        rx="8"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></rect>
+                      <circle cx="68" cy="84" r="12"></circle>
+                      <circle cx="108" cy="84" r="12"></circle>
+                    </svg>
+                  </div>
+                  <div className="service-grid-icon-name">
+                    All kind of Websites
+                  </div>
+                </div>
+                <div className="service-page-grid-items">
+                  <div className="service-page-grid-icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      className="icon-service-1"
+                      fill="#000000"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <path
+                        d="M184,184H69.8L41.9,30.6A8,8,0,0,0,34.1,24H16"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                      <circle
+                        cx="80"
+                        cy="204"
+                        r="20"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></circle>
+                      <circle
+                        cx="184"
+                        cy="204"
+                        r="20"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></circle>
+                      <path
+                        d="M62.5,144H188.1a15.9,15.9,0,0,0,15.7-13.1L216,64H48"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="service-grid-icon-name">
+                    WooCommerce Web App
+                  </div>
+                </div>
+                <div className="service-page-grid-items">
+                  <div className="service-page-grid-icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      className="icon-service-1"
+                      fill="#000000"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <rect
+                        x="64"
+                        y="24"
+                        width="128"
+                        height="208"
+                        rx="16"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></rect>
+                      <line
+                        x1="64"
+                        y1="56"
+                        x2="192"
+                        y2="56"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                      <line
+                        x1="64"
+                        y1="200"
+                        x2="192"
+                        y2="200"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                    </svg>
+                  </div>
+                  <div className="service-grid-icon-name">
+                    Mobile App Development
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article>
+          <div className="service-page-main-3">
+            <div className="pa-1">
+              <div className="pa-2">
+                <img className="pa-images" src={Uxui} alt="UX/UI development" />
+                <span className="pa-sub-1-title">UI/UX Development</span>
+                <br></br>
+                <span className="pa-sub-1-body">body-1</span>
+              </div>
+              <div className="pa-2">
+                <span className="pa-sub-1-title">UI/UX Development</span>
+                <br></br>
+                <span className="pa-sub-1-body">body-1</span>
+                <img
+                  className="pa-images-1"
+                  src={Webpage}
+                  alt="Webpage Creation"
+                />
+              </div>
+
+              <div className="pa-2">
+                <img
+                  className="pa-images"
+                  src={admin}
+                  alt="Admin panel management"
+                />
+                <span className="pa-sub-1-title">UI/UX Development</span>
+                <br></br>
+                <span className="pa-sub-1-body">body-1</span>
+              </div>
+              <div className="pa-2">
+                <span className="pa-sub-1-title">UI/UX Development</span>
+                <br></br>
+                <span className="pa-sub-1-body">body-1</span>
+                <img
+                  className="pa-images-1"
+                  src={mobileApp}
+                  alt="Mobile app development"
+                />
+              </div>
+              <div className="pa-2">
+                <img
+                  className="pa-images"
+                  src={wooCommerce}
+                  alt="Woo Commerce web app"
+                />
+                <span className="pa-sub-1-title">UI/UX Development</span>
+                <br></br>
+                <span className="pa-sub-1-body">body-1</span>
+              </div>
+              <div className="pa-2">
+                <span className="pa-sub-1-title">UI/UX Development</span>
+                <br></br>
+                <span className="pa-sub-1-body">body-1</span>
+                <img className="pa-images-1" src={website} alt="websites" />
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
